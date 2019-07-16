8ac7f01347986ee1f70b147d587877116bcbb419
Corey Noble <coreynoble@hotmail.ca>
2019-07-15 23:02:25 -0400

    30 - JSX Restrictions

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      // JSX expression must have one root element.
       <div className="App">
         <h1>Hi, I'm a React App</h1>
+        <p>Child element.</p>
       </div>
+      // Cant add another <h1> below.
+      // <p>!- Won't work</p>
     );
+
+    // The JSX code above compiles to the below inside of 'React Component'.
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));
   }
 }
 
