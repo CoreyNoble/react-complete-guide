5531b306078e2b8cb8a90d8536637615d5526fe2
Corey Noble <coreynoble@hotmail.ca>
2019-07-16 22:25:12 -0400

    35 - Working with Props

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>Child element.</p>
-        <Person />
-        <Person />
-        <Person />
+        <Person name="Corey" age="26" />
+        <Person name="Kaitlin" age="26">My Hobbies: Reading</Person>
+        <Person name="Dante" age="5" />
       </div>
       // Cant add another <h1> below.
       // <p>!- Won't work</p>
