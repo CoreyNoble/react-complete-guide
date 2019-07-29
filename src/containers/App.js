aa38cbb947f1cd8598d67b23e51118d70a4df8eb
Corey Noble <coreynoble@hotmail.ca>
2019-07-29 13:29:31 -0400

    90 - Using useEffect() in Functional Components

--- src/containers/App.js
+++ src/containers/App.js
@@ -29,7 +29,16 @@ class App extends Component {
   // }
 
   componentDidMount() {
-    console.log('[App.js] componentDidMount')
+    console.log('[App.js] componentDidMount');
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[App.js] shouldComponentUpdate');
+    return true;
+  }
+
+  componentDidUpdate() {
+    console.log('[App.js] componentDidUpdate');
   }
 
   nameChangedHandler = (event, id) => {
