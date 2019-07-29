e62de563dd359c43d4387a89cfa2aa963e334854
Corey Noble <coreynoble@hotmail.ca>
2019-07-29 18:50:14 -0400

    102 - Resetting React.Fragment back to Auxillary for next lecture

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import classes from './Person.css';
 import Auxillary from '../../../hoc/Auxillary';
 
@@ -6,7 +6,7 @@ class Person extends Component {
     render() {
         console.log('[Person.js] rendering...')
         return (
-            <Fragment>
+            <Auxillary>
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p >{this.props.children}</p>
                 <input 
@@ -14,7 +14,7 @@ class Person extends Component {
                     onChange={this.props.changed} 
                     value={this.props.name} 
                 />
-            </Fragment>
+            </Auxillary>
         )
     }
 };
