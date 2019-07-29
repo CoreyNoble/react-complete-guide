7d437cc9bcfb190f8f0998a7148072a802e930be
Corey Noble <coreynoble@hotmail.ca>
2019-07-29 15:45:37 -0400

    93 - Cleaning up with Lifecycle Hooks & useEffect()

--- src/containers/App.js
+++ src/containers/App.js
@@ -17,6 +17,7 @@ class App extends Component {
     ],
     otherState: 'some other value',
     showPersons: false,
+    showCockpit: true,
   }
 
   static getDerivedStateFromProps(props, state) {
@@ -82,11 +83,20 @@ class App extends Component {
 
     return (
       <div className={classes.App}>
-        <Cockpit 
-          title={this.props.appTitle}
-          showPersons={this.state.showPersons}
-          persons={this.state.persons}
-          clicked={this.togglePersonsHandler} />
+        <button onClick={() => {
+          this.setState({showCockpit: false});
+          }}
+        >
+            Remove Cockpit
+        </button>
+        {this.state.showCockpit ? (
+          <Cockpit 
+            title={this.props.appTitle}
+            showPersons={this.state.showPersons}
+            persons={this.state.persons}
+            clicked={this.togglePersonsHandler} 
+          />
+        ) : null}
         {persons}
       </div>
     );
