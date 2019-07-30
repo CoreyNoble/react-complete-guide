3ff2b913fb40195426ae614c303d6a5ebfdaa14f
Corey Noble <coreynoble@hotmail.ca>
2019-07-29 21:09:34 -0400

    109 - Refs with React Hooks

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,13 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import classes from './Cockpit.css';
 
 const cockpit = props => {
+    const toggleBtnRef = useRef(null);
+
     useEffect(() => {
         console.log('[Cockpit.js] useEffect');
         // Http request...
-        setTimeout(() => {
-            alert('Saved data to cloud!');
-        }, 1000);
+        // setTimeout(() => {
+        //     alert('Saved data to cloud!');
+        // }, 1000);
+        toggleBtnRef.current.click();
         return () => {
             console.log('[Cockpit.js] cleanup work in useEffect');
         };
@@ -20,26 +23,26 @@ const cockpit = props => {
         };
     });
 
-    const assignedClasses = []
+    const assignedClasses = [];
     let btnClass = '';
-    if (props.ShowPersons) {
+    if (props.showPersons) {
         btnClass = classes.Red;
     }
 
     if (props.personsLength <= 2) {
-      assignedClasses.push( classes.red );
+      assignedClasses.push(classes.red);
     }
     if (props.personsLength <= 1) {
-      assignedClasses.push( classes.bold );
+      assignedClasses.push(classes.bold);
     }
 
     return (
         <div className={classes.Cockpit}>
             <h1>{props.title}</h1>
             <p className={assignedClasses.join(' ')}>This is really working!</p>
-            <button 
-            className={btnClass}
-            onClick={props.clicked}>Toggle Persons</button>
+            <button ref={toggleBtnRef} className={btnClass} onClick={props.clicked}>
+                Toggle Persons
+            </button>
         </div>
     );
 };
