08c2fe72c8006c4ff4f34c1f09670855a373390e
Corey Noble <coreynoble@hotmail.ca>
2019-08-18 19:02:35 -0400

    291 - Updating Arrays Immutably (<li> key not working)

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -31,6 +31,10 @@ const reducer = (state = initialState, action) => {
                 results: state.results.concat({id: new Date(), value: state.counter})
             }
         case 'DELETE_RESULT':
+            // const id = 2;
+            // const newArray = [...state.results];
+            // newArray.splice(id, 1);
+            const updatedArray = state.results.filter((result, index) => result.id !== action.resultElId);
             return {
                 ...state,
             }
