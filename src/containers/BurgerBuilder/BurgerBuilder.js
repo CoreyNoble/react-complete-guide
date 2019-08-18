957bdbdd9c44ac1fdd8dde76128357c736576083
Corey Noble <coreynoble@hotmail.ca>
2019-08-17 20:08:04 -0400

    250 - Navigating to the Checkout Page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -92,29 +92,30 @@ class BurgerBuilder extends Component{
 
     purchaseContinueHandler = () => {
         // alert('You continue!');
-        this.setState({loading: true});
+        // this.setState({loading: true});
 
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Corey Noble',
-                address: {
-                    street: '5170 Lakeshore Rd, Burlington, ON',
-                    zipCode: 'L7L1C4',
-                    country: 'Canada'
-                },
-                email: 'coreynoble@hotmail.ca'
-            },
-            deliveryMethod: 'fastest'
-        }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({loading: false, purchasing: false});
-            })
-            .catch(error => {
-                this.setState({loading: false, purchasing: false});
-            });
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Corey Noble',
+        //         address: {
+        //             street: '5170 Lakeshore Rd, Burlington, ON',
+        //             zipCode: 'L7L1C4',
+        //             country: 'Canada'
+        //         },
+        //         email: 'coreynoble@hotmail.ca'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // }
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         this.setState({loading: false, purchasing: false});
+        //     })
+        //     .catch(error => {
+        //         this.setState({loading: false, purchasing: false});
+        //     });
+        this.props.history.push('/checkout');
     }
 
     render() {
