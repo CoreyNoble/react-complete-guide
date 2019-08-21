4bc75b3dbd0e5cade48d9199b9af4674f7fd25e3
Corey Noble <coreynoble@hotmail.ca>
2019-08-20 23:08:29 -0400

    303 - Saving ALL

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -31,13 +31,13 @@ class BurgerBuilder extends Component{
     }
 
     componentDidMount () {
-        axios.get('https://react-my-burger-7c324.firebaseio.com/ingredients.json')
-            .then(response => {
-                this.setState({ingredients: response.data});
-            })
-            .catch(error => {
-                this.setState({error: true});
-            });
+        // axios.get('https://react-my-burger-7c324.firebaseio.com/ingredients.json')
+        //     .then(response => {
+        //         this.setState({ingredients: response.data});
+        //     })
+        //     .catch(error => {
+        //         this.setState({error: true});
+        //     });
     }
 
     updatePurchaseState (ingredients) {
