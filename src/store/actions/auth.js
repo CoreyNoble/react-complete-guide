ab7737d173e05dbc00af5ab323bc57c566bdd25c
Corey Noble <coreynoble@hotmail.ca>
2019-08-24 11:21:32 -0400

    355 - Adding Sign-In

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -21,15 +21,19 @@ export const authFail = (error) => {
     };
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
         const authData = {
             email: email,
             password: password,
             returnSecureToken: true
+        };
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCgsbkJM-unwKqYALm2wckm7NcvTX2IK7k';
+        if (!isSignup) {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCgsbkJM-unwKqYALm2wckm7NcvTX2IK7k';
         }
-        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCgsbkJM-unwKqYALm2wckm7NcvTX2IK7k', authData)
+        axios.post(url, authData)
             .then(response => {
                 console.log(response);
                 dispatch(authSuccess(response.data));
