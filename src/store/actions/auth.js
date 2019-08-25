f08d7d88b6aec1355b4b5af4c255c5c2018b0b13
Corey Noble <coreynoble@hotmail.ca>
2019-08-25 12:52:38 -0400

    363 - Redirecting the User to the Checkout Page - With ERROR

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -59,3 +59,10 @@ export const auth = (email, password, isSignup) => {
             });
     };
 };
+
+export const setAuthResirectPath = (path) => {
+    return {
+        type: actionTypes.SET_AUTH_REDIRECT_PATH,
+        path: path
+    }
+};
