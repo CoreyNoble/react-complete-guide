d1715ae35eb019297b26a3772503a26cb98ef72b
Corey Noble <coreynoble@hotmail.ca>
2019-09-14 20:43:30 -0400

    468 - Understanding useEffect() Dependencies

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -24,6 +24,10 @@ const Ingredients = () => {
       });
   }, []);
 
+  useEffect(() => {
+    console.log('RENDERING INGREDIENTS', userIngredients);
+  }, [userIngredients]);
+
   const addIngredientHandler = ingredient => {
     fetch('https://react-hooks-update-3da8b.firebaseio.com/ingredients.json', {
       method: 'POST',
