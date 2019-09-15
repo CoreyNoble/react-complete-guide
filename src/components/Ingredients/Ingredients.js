f3045be3cdd11786b2bc6231ebe78e22e3949bfb
Corey Noble <coreynoble@hotmail.ca>
2019-09-14 20:41:02 -0400

    467 - useEffect() & Loading Data

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,11 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import Search from './Search';
 
 const Ingredients = () => {
-  const [ userIngredients, setUserIngredients ] = useState([]);
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  useEffect(() => {
+    fetch('https://react-hooks-update-3da8b.firebaseio.com/ingredients.json')
+      .then(response => response.json())
+      .then(responseData => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount
+          });
+        }
+
+        setUserIngredients(loadedIngredients);
+      });
+  }, []);
 
   const addIngredientHandler = ingredient => {
     fetch('https://react-hooks-update-3da8b.firebaseio.com/ingredients.json', {
